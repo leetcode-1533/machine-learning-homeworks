8a1116382c1e7961cb75a0b65a884e92a640de79
Yingjie <1275963@gmail.com>
2016-03-24 14:07:01 +0800

    Step 1

--- projects/boston_housing/pro.py
+++ projects/boston_housing/pro.py
@@ -4,9 +4,6 @@ import matplotlib.pyplot as pl
 from sklearn import datasets
 from sklearn.tree import DecisionTreeRegressor
 
-# Make matplotlib show our plots inline (nicely formatted in the notebook)
-%matplotlib inline
-
 # Create our client's feature set for which we will be predicting a selling price
 CLIENT_FEATURES = [[11.95, 0.00, 18.100, 0, 0.6590, 5.6090, 90.00, 1.385, 24, 680.0, 20.20, 332.09, 12.13]]
 
@@ -18,3 +15,35 @@ housing_prices = city_data.target
 housing_features = city_data.data
 
 print "Boston Housing dataset loaded successfully!"
+
+# Number of houses in the dataset
+total_houses = housing_features.shape[0]
+
+
+# Number of features in the dataset
+total_features = housing_features.shape[1]
+
+# Minimum housing value in the dataset
+minimum_price = housing_prices.min()
+
+# Maximum housing value in the dataset
+maximum_price = housing_prices.max()
+
+# Mean house value of the dataset
+mean_price = housing_prices.mean()
+
+# Median house value of the dataset
+median_price = np.median(np.array(housing_prices))
+
+# Standard deviation of housing values of the dataset
+std_dev = np.std(np.array(housing_prices))
+
+# Show the calculated statistics
+print "Boston Housing dataset statistics (in $1000's):\n"
+print "Total number of houses:", total_houses
+print "Total number of features:", total_features
+print "Minimum house price:", minimum_price
+print "Maximum house price:", maximum_price
+print "Mean house price: {0:.3}".format(mean_price)
+print "Median house price:", median_price
+print "Standard deviation of house price: {0:.3}".format(std_dev)
