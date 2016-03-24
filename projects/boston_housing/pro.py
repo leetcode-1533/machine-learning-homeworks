911b7da4dd96f99e0a76d86271b40074ddbb54ee
Yingjie <1275963@gmail.com>
2016-03-24 15:06:43 +0800

    Shuffle finished

--- projects/boston_housing/pro.py
+++ projects/boston_housing/pro.py
@@ -47,3 +47,33 @@ print "Maximum house price:", maximum_price
 print "Mean house price: {0:.3}".format(mean_price)
 print "Median house price:", median_price
 print "Standard deviation of house price: {0:.3}".format(std_dev)
+
+# Put any import statements you need for this code block here
+
+def shuffle_split_data(X, y):
+    """ Shuffles and splits data into 70% training and 30% testing subsets,
+        then returns the training and testing subsets. """
+
+    # Shuffle and split the data  
+    from sklearn import cross_validation   
+    rs = cross_validation.ShuffleSplit(X.shape[0], n_iter=1, test_size= 0.3)
+    rs = list(rs)[0]
+    train_index = rs[0]
+    test_index = rs[1]
+    
+    X_train = X[train_index]
+    y_train = y[train_index]
+    
+    X_test = X[test_index]
+    y_test = y[test_index]
+
+    # Return the training and testing data subsets
+    return X_train, y_train, X_test, y_test
+
+
+# Test shuffle_split_data
+try:
+    X_train, y_train, X_test, y_test = shuffle_split_data(housing_features, housing_prices)
+    print "Successfully shuffled and split the data!"
+except:
+    print "Something went wrong with shuffling and splitting the data."
